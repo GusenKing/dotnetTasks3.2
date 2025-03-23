c3c53f9381c9a426069e6728f14ef30174ecc607
Emin <emin.sarux2004@yandex.ru>
2025-03-23 13:10:35 +0300

    whatever

--- Lesson1.GraphQL/Lesson1.GraphQL/obj/Debug/net8.0/Lesson1.GraphQL.AssemblyInfo.cs
+++ Lesson1.GraphQL/Lesson1.GraphQL/obj/Debug/net8.0/Lesson1.GraphQL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lesson1.GraphQL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12d278f0b31b7423cf6120601f85175f997ff217")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lesson1.GraphQL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lesson1.GraphQL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
