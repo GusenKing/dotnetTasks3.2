dc66a8df5892b4528f4d7e362dfe9d0e12adae09
Emin <emin.sarux2004@yandex.ru>
2025-03-31 10:37:42 +0300

    it useless too

---
+++ Lesson2.Grpc/Task1.Client/Program.cs
@@ -0,0 +1,14 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Grpc.Net.Client;
+using Lesson2.Grpc;
+
+var channel = GrpcChannel.ForAddress("https://localhost:7069");
+var client = new WeatherService.WeatherServiceClient(channel);
+using var call = client.StreamingFromServer(new WeatherStreamRequest
+    { CurrentTime = Timestamp.FromDateTimeOffset(DateTimeOffset.Now) });
+
+await foreach (var response in
+               call.ResponseStream.ReadAllAsync())
+    Console.WriteLine(
+        $"{response.Time} = {response.WeatherValue} {response.WeatherUnits}");
